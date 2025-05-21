15c397679402887517131b469582c8c36834e4b1
andrei.iacob <andrei.iacob@arnia.ro>
2025-05-21 14:13:36 +0300

    Added new example.

---
+++ VirtualPdfPrinterPSA1/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace VirtualPdfPrinterPSA
+{
+    public sealed partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Updated to launch the WPF helper (Full Trust Process)
+        /// </summary>
+        protected override async void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var logPath = @"C:\Work\DocuWare\docuware-v2\Logs\psa-invoked.txt";
+            Directory.CreateDirectory(Path.GetDirectoryName(logPath));
+            File.AppendAllText(logPath, $"UWP PSA launched at {DateTime.Now}\r\n");
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            deferral.Complete();
+        }
+    }
+}
