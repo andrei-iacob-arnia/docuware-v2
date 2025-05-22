4699652387503130812fb5d61b103ea01c180da7
andrei.iacob <andrei.iacob@arnia.ro>
2025-05-22 15:05:24 +0300

    Added new project.

---
+++ VirtualPdfPrinterWinUI/VirtualPrinterBackgroundTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Windows.ApplicationModel.Background;
+
+namespace VirtualPdfPrinterWinUI
+{
+    public sealed class VirtualPrinterBackgroundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            try
+            {
+                var logFile = @"C:\Work\DocuWare\docuware-v2\Logs\psa-invoked.txt";
+                Directory.CreateDirectory(System.IO.Path.GetDirectoryName(logFile));
+                File.AppendAllText(logFile, $"VirtualPrinterBackgroundTask launched at {DateTime.Now}\r\n");
+            }
+            finally
+            {
+                taskInstance.GetDeferral().Complete();
+            }
+        }
+    }
+}
